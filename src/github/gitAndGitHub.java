bd8b2bc346f7650a98de391e6c51c566c9015253
HurstJake1024 <hurstjake1024@gmail.com>
2022-06-10 19:58:19 -0400

    2nd commit

--- src/github/gitAndGitHub.java
+++ src/github/gitAndGitHub.java
@@ -5,6 +5,7 @@ public class gitAndGitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("today we learn github");
+		System.out.println("today is a very good day");
 		
 
 	}
